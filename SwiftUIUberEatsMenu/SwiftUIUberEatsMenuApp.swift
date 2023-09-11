0087ab7cc0f811e33d57a9a385e3b6574bcb08ba
philparham <phil.parham@digital-tectonics.com>
2023-09-11 17:20:03 -0400

    Initial Commit

---
+++ SwiftUIUberEatsMenu/SwiftUIUberEatsMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIUberEatsMenuApp.swift
+//  SwiftUIUberEatsMenu
+//
+//  Created by Ramar Parham on 9/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIUberEatsMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
